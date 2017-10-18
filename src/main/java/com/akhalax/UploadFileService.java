57770f4c0bcab98dea52fd64f2a7a3ca51df59e7
Akhalax <isalliok@gmail.com>
2017-10-18 17:17:32 +0300

    Creating new branch

--- src/main/java/com/akhalax/UploadFileService.java
+++ src/main/java/com/akhalax/UploadFileService.java
@@ -4,10 +4,7 @@ import org.glassfish.jersey.media.multipart.ContentDisposition;
 import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 import org.glassfish.jersey.media.multipart.FormDataParam;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.*;
